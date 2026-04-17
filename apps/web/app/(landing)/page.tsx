7fd93c6160773424800cc52a4dc30baf7ad0a395
Rowee13 <roweeapor@gmail.com>
2026-04-17 10:55:32 +0800

    feat(web): add feature wishlist section with github discussions fetch

--- apps/web/app/(landing)/page.tsx
+++ apps/web/app/(landing)/page.tsx
@@ -1,6 +1,7 @@
 import { HeroSection } from '../../components/landing/HeroSection';
 import { ToolShowcaseSection } from '../../components/landing/ToolShowcaseSection';
 import { ValuePropsSection } from '../../components/landing/ValuePropsSection';
+import { FeatureWishlistSection } from '../../components/landing/FeatureWishlistSection';
 
 export default function LandingPage() {
   return (
@@ -8,6 +9,7 @@ export default function LandingPage() {
       <HeroSection />
       <ToolShowcaseSection />
       <ValuePropsSection />
+      <FeatureWishlistSection />
     </>
   );
 }
