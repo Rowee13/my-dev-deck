2785f4b3cf2acabe264e496651e62b733680b83c
Rowee13 <roweeapor@gmail.com>
2026-04-17 10:50:45 +0800

    feat(web): add value props section with staggered reveal animation

--- apps/web/app/(landing)/page.tsx
+++ apps/web/app/(landing)/page.tsx
@@ -1,11 +1,13 @@
 import { HeroSection } from '../../components/landing/HeroSection';
 import { ToolShowcaseSection } from '../../components/landing/ToolShowcaseSection';
+import { ValuePropsSection } from '../../components/landing/ValuePropsSection';
 
 export default function LandingPage() {
   return (
     <>
       <HeroSection />
       <ToolShowcaseSection />
+      <ValuePropsSection />
     </>
   );
 }
